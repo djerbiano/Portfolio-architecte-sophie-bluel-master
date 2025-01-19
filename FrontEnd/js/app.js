7069af851e2888b336c6598573fc713f8ec9adda
djerbiano <saberghoudi2222@hotmail.fr>
2025-01-19 22:07:40 +0100

    Fix filter logic to use ID and complete project creation and deletion functionality

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,154 +1,344 @@
 import { displayProjects, formatTitle } from "./utils.js";
+const token = sessionStorage.getItem("token");
+const modeLoginElementsToShow = document.querySelectorAll(".login");
+const modeLoginElementsToHide = document.querySelectorAll(".logout");
+const barModeUpdate = document.querySelector(".barModeUpdate");
+const btnUpdateProject = document.querySelector(".editProject");
+const modalEditProject = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".closeIcon");
+const btnCloseModalAddPhoto = document.querySelector(".closeIconAddPhoto");
+const ContainerProjectInModalToUpdate = document.querySelector(
+  ".projectImgContainer"
+);
+const infoModal = document.querySelector(".infoModal");
+const infoModalContent = document.querySelector(".infoModal p");
+const btnCloseInfoModal = document.querySelector(".closeInfoModal");
+const btnLogout = document.querySelector(".toLogout");
+const modalAddPhoto = document.querySelector(".modalPhoto");
+const modalDeletePhoto = document.querySelector(".showData");
+const btnAddPhoto = document.querySelector(".btnAddPhoto");
+const backIconModalPhoto = document.querySelector(".backIcon");
+const ContainerCategoryToAddPhoto = document.querySelector("#category");
+// show photo before upload
+const btnUploadPhoto = document.querySelector(".photoUpload button");
+const inputFile = document.querySelector(".inputFile");
+const previewImage = document.querySelector("#previewImage");
+const showErrorMessage = document.querySelector(".errorMessage");
+const btnSendNewProject = document.querySelector(".addPhoto");
 
 // Api address
 const API_URL = (API_ENDPOINT) => `http://localhost:5678/api/${API_ENDPOINT}`;
 
 // get all projects
 let projectsData = [];
+let uniqueCategories = [];
 
-// get all projects and create buttons
-const generateFilterBtnsAndProjects = async () => {
-  const containerFilterBtn = document.querySelector(".filterContainer");
-
+// get all projects
+const getProjects = async () => {
   try {
     const response = await fetch(API_URL("works"));
 
     if (!response.ok) {
-      console.log(response);
       throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
     }
 
     projectsData = await response.json();
 
-    containerFilterBtn.innerHTML = "";
-
-    // unique categories
-    const uniqueCategories = [
-      ...new Set(projectsData.map((project) => project.category.name)),
-    ];
-
     // display all projects by default
     displayProjects(projectsData);
-
-    // display buttons
-    containerFilterBtn.innerHTML = `<button class="btn">Tous</button>`;
-    uniqueCategories.forEach((categoryName) => {
-      containerFilterBtn.innerHTML += `
-        <button class="btn">${categoryName}</button>
-      `;
-    });
-
-    // filter projects by category
-    const filterBtns = document.querySelectorAll(".btn");
-    filterBtns.forEach((btn) => {
-      btn.addEventListener("click", (e) => {
-        const categoryName = e.target.textContent;
-        if (categoryName === "Tous") {
-          displayProjects(projectsData);
-        } else {
-          filterProjects(categoryName);
-        }
-      });
-    });
   } catch (error) {
     console.error("Erreur lors de la récupération des projets :", error);
   }
 };
 
-generateFilterBtnsAndProjects();
+getProjects();
 
-// Filter projects
-const filterProjects = (categoryName) => {
+const generateCategoryButton = async () => {
+  const containerFilterBtn = document.querySelector(".filterContainer");
+  try {
+    const response = await fetch(API_URL("categories"));
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
+    }
+
+    uniqueCategories = await response.json();
+    containerFilterBtn.innerHTML = "";
+
+    // display buttons
+    containerFilterBtn.innerHTML = `<button class="btn" data-categoryId="Tous">Tous</button>`;
+    uniqueCategories.forEach((categoryName) => {
+      containerFilterBtn.innerHTML += `
+        <button class="btn" data-categoryId="${categoryName.id}">${categoryName.name}</button>
+      `;
+    });
+    filteredProjects();
+  } catch (error) {
+    console.error("Erreur lors de la récupération des catégories :", error);
+  }
+};
+
+generateCategoryButton();
+
+const handleFilterProjects = (categoryId) => {
   const filteredProjects = projectsData.filter(
-    (project) => project.category.name === categoryName
+    (project) => project.category.id === categoryId
   );
   displayProjects(filteredProjects);
 };
 
-// Display or hide elements based on login status
-const token = sessionStorage.getItem("token");
-const elementsToShow = document.querySelectorAll(".login");
-const elementsToHide = document.querySelectorAll(".logout");
+// filteredProjects
+const filteredProjects = () => {
+  // filter projects by categoryId
+  const filterBtns = document.querySelectorAll(".btn");
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const categoryId =
+        e.target.textContent === "Tous"
+          ? "Tous"
+          : Number(e.target.getAttribute("data-categoryId"));
+      if (categoryId == "Tous") {
+        displayProjects(projectsData);
+      } else {
+        handleFilterProjects(categoryId);
+      }
+    });
+  });
+};
 
+// Display or hide elements based on login status
 if (token) {
-  elementsToShow.forEach((element) => {
+  modeLoginElementsToShow.forEach((element) => {
     element.style.display = "flex";
   });
-  elementsToHide.forEach((element) => {
+  modeLoginElementsToHide.forEach((element) => {
     element.style.display = "none";
   });
 }
-// -----------------
-//start open modal to edit project
-// ------------------
-const btnUpdateProject = document.querySelector(".editProject");
-const modalEditProject = document.querySelector(".modal");
-const btnCloseModal = document.querySelector(".closeIcon");
-const barModeUpdate = document.querySelector(".barModeUpdate");
+const cleanFieldsInModalAddPhoto = () => {
+  document.querySelector("#title").value = "";
+  document.querySelector("#category").value = "";
+  previewImage.src = "";
+  previewImage.style.display = "none";
+  btnUploadPhoto.style.opacity = 1;
+  inputFile.value = "";
+};
 
+const showApiResponse = (message) => {
+  infoModal.classList.add("showInfo");
+  infoModalContent.textContent = message;
+};
+// open modal to edit project
 btnUpdateProject.addEventListener("click", () => {
-  // modalEditProject.style.display = "flex";
   modalEditProject.classList.add("show");
   window.addEventListener("click", handleClickOutsideModal);
+  btnAddPhoto.addEventListener("click", handleOpenPhotoModal);
+  backIconModalPhoto.addEventListener("click", handleClosePhotoModal);
+  inputFile.addEventListener("change", showPhotoBeforeUpload);
+  btnSendNewProject.addEventListener("click", createProject);
   displayProjectInModal();
+
+  // clean input fields
+  cleanFieldsInModalAddPhoto();
 });
 
-// close modal edit project
+// close modal edit project and remove all eventListener
 const closeModal = () => {
   modalEditProject.classList.remove("show");
   window.removeEventListener("click", handleClickOutsideModal);
+  ContainerProjectInModalToUpdate.removeEventListener(
+    "click",
+    handleDeleteProject
+  );
+  btnCloseInfoModal.removeEventListener("click", handleCloseInfoModal);
+  btnAddPhoto.removeEventListener("click", handleOpenPhotoModal);
+  backIconModalPhoto.removeEventListener("click", handleClosePhotoModal);
+  inputFile.removeEventListener("change", showPhotoBeforeUpload);
+  btnSendNewProject.removeEventListener("click", createProject);
 };
+
+// close modal when click outside or on close icon or bar mode update
 const handleClickOutsideModal = (e) => {
+  // verify that info modal is not open
+  if (infoModal.classList.contains("showInfo")) {
+    return;
+  }
   if (
     e.target === modalEditProject ||
     e.target === btnCloseModal ||
-    barModeUpdate.contains(e.target)
+    barModeUpdate.contains(e.target) ||
+    e.target === btnCloseModalAddPhoto
   ) {
     closeModal();
+    modalAddPhoto.style.display = "none";
+    modalDeletePhoto.style.display = "flex";
   }
 };
 
-// display project in modal
 const displayProjectInModal = () => {
-  const projectImgContainer = document.querySelector(".projectImgContainer");
-  projectImgContainer.innerHTML = "";
+  ContainerProjectInModalToUpdate.innerHTML = "";
+  if (projectsData.length < 1) {
+    ContainerProjectInModalToUpdate.innerHTML = `<p class="noProject"> Aucun projet disponible </p>`;
+    return;
+  }
   projectsData.forEach((project) => {
     const formattedTitle = formatTitle(project.title);
-    projectImgContainer.innerHTML += `
+    ContainerProjectInModalToUpdate.innerHTML += `
         <div class="project">
           <img src="${project.imageUrl}" alt="${formattedTitle}"/>
           <span class="material-symbols-outlined deleteIcon" data-id="${project.id}"> delete </span>
           </div>
           `;
   });
-  handleDeleteProject();
+
+  // delete project from modal
+  ContainerProjectInModalToUpdate.addEventListener(
+    "click",
+    handleDeleteProject
+  );
+};
+
+// get id to delete project
+const handleDeleteProject = (e) => {
+  if (e.target.classList.contains("deleteIcon")) {
+    const dataId = e.target.getAttribute("data-id");
+    deleteProject(dataId);
+  }
 };
 
 // delete project
-const handleDeleteProject = () => {
-  const deleteIcon = document.querySelectorAll(".deleteIcon ");
-  const infoModal = document.querySelector(".infoModal");
-  const infoModalContent = document.querySelector(".infoModal p");
-  deleteIcon.forEach((icon) => {
-    icon.addEventListener("click", () => {
-      const dataId = icon.getAttribute("data-id");
-      // open modal info
-      infoModal.classList.add("showInfo");
-      infoModalContent.textContent = ` Votre projet n° ${dataId} a bien été supprimé !`;
+const deleteProject = async (id) => {
+  // verify that info modal is not open
+  if (infoModal.classList.contains("showInfo")) {
+    return;
+  }
+
+  try {
+    const response = await fetch(API_URL(`works/${id}`), {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
-  });
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
+    }
+
+    // open modal and display message
+    showApiResponse(`Votre projet n° ${id} a bien été supprimé !`);
+  } catch (error) {
+    console.error("Erreur lors de la suppression du projet :", error);
+  }
   // close info modal
-  const btnCloseModalInfo = document.querySelector(".closeInfoModal");
-  btnCloseModalInfo.addEventListener("click", () => {
-    infoModal.classList.remove("showInfo");
+  btnCloseInfoModal.addEventListener("click", handleCloseInfoModal);
+
+  // display new list projects without refresh page
+  await getProjects();
+  displayProjectInModal();
+};
+
+const handleCloseInfoModal = () => {
+  infoModal.classList.remove("showInfo");
+};
+
+// open modal to add photo
+const handleOpenPhotoModal = () => {
+  modalAddPhoto.style.display = "flex";
+  modalDeletePhoto.style.display = "none";
+  generateCategoryToAddPhoto();
+};
+
+const handleClosePhotoModal = () => {
+  // verify that info modal is not open
+  if (infoModal.classList.contains("showInfo")) {
+    return;
+  }
+  modalAddPhoto.style.display = "none";
+  modalDeletePhoto.style.display = "flex";
+};
+
+const generateCategoryToAddPhoto = () => {
+  ContainerCategoryToAddPhoto.innerHTML = ``;
+  ContainerCategoryToAddPhoto.innerHTML += `<option value="">Choisir une catégorie</option>`;
+  uniqueCategories.forEach((category) => {
+    ContainerCategoryToAddPhoto.innerHTML += `<option value="${category.id}">${category.name}</option>`;
   });
 };
+
+const showPhotoBeforeUpload = () => {
+  if (inputFile.files[0]) {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      previewImage.src = reader.result;
+      previewImage.style.display = "block";
+      previewImage.alt = inputFile.files[0].name;
+    });
+    reader.readAsDataURL(inputFile.files[0]);
+    btnUploadPhoto.style.opacity = 0;
+  }
+};
+
+// add new project
+const createProject = async () => {
+  const title = document.querySelector("#title").value;
+  const categoryId = document.querySelector("#category").value;
+  showErrorMessage.textContent = "";
+
+  // verify data befor upload
+  if (!title) {
+    showErrorMessage.textContent = "Veuillez choisir du titre";
+    return;
+  } else if (!inputFile.files[0]) {
+    showErrorMessage.textContent = "Veuillez sélectionner une photo";
+    return;
+  } else if (!categoryId) {
+    showErrorMessage.textContent = "Veuillez choisir une catégorie";
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append("title", title);
+  formData.append("image", inputFile.files[0]);
+  formData.append("category", categoryId);
+
+  try {
+    const response = await fetch(API_URL("works"), {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+    if (response.status !== 201) {
+      throw new Error(`Erreur HTTP ! Statut : ${response}`);
+    }
+
+    const data = await response.json();
+
+    // open modal and display message
+    showApiResponse(`Votre projet ${data.title} a bien été ajouté !`);
+
+    // clean input fields after upload
+    cleanFieldsInModalAddPhoto();
+
+    // display new list projects without refresh page
+    await getProjects();
+    displayProjectInModal();
+  } catch (error) {
+    console.error("Erreur lors de l'ajout du projet :", error);
+  }
+
+  // close info modal
+  btnCloseInfoModal.addEventListener("click", handleCloseInfoModal);
+};
+
+// desactivate form if modal info is open
 // -----------------
-// end open modal to edit project
+//
 // ------------------
 
 // logout
-const btnLogout = document.querySelector(".toLogout");
 btnLogout.addEventListener("click", () => {
   sessionStorage.removeItem("token");
   window.location.href = "index.html";
--- FrontEnd/js/utils.js
+++ FrontEnd/js/utils.js
@@ -1,16 +1,19 @@
-
 // replace space with dash
 export const formatTitle = (title) => {
-    return title.replace(/\s+/g, "-");
-  };
+  return title.replace(/\s+/g, "-");
+};
 
 // Function to display projects (default and filtered)
 export const displayProjects = (projects) => {
-    const containerCard = document.querySelector(".gallery");
-    containerCard.innerHTML = "";
-    projects.forEach((work) => {
-      const formattedTitle = formatTitle(work.title);
-      containerCard.innerHTML += `
+  const containerCard = document.querySelector(".gallery");
+  containerCard.innerHTML = "";
+  if (projects.length < 1) {
+    containerCard.innerHTML = `<p class="noProject"> Aucun projet disponible </p>`;
+    return;
+  }
+  projects.forEach((work) => {
+    const formattedTitle = formatTitle(work.title);
+    containerCard.innerHTML += `
         <div class="galleryCard">
           <figure>
             <img src="${work.imageUrl}" alt="${formattedTitle}">
@@ -18,6 +21,7 @@ export const displayProjects = (projects) => {
           </figure>
         </div>
       `;
-    });
-  };
-    
+  });
+};
+
+ 
